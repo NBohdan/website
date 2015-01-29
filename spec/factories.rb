178f091cc4a64e0d8dc482b0aefbfdc265a3237c
Grey <nbohdani@gmail.com>
2015-01-29 16:25:33 +0200

    &1

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Bohdan Nedostup"
+    email    "nbohdani@mail.ru"
+    password "greenfield"
+    password_confirmation "greenfield"
+  end
+end
