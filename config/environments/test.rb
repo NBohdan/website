28ad643dd7dbca1aedf03980babb35416774d8d2
Grey <nbohdani@gmail.com>
2015-01-29 19:12:17 +0200

    &2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ Website::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering bcrypt's cost function.
+  ActiveModel::SecurePassword.min_cost = true
 end
