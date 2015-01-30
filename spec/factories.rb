2b08900064c9cf08df6dab9032f4efd78b9d7b17
Grey <nbohdani@gmail.com>
2015-01-30 02:26:37 +0200

    (

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Bohdan Nedostup"
-    email    "nbohdani@mail.ru"
-    password "greenfield"
-    password_confirmation "greenfield"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+    	admin true
+    end
   end
 end
